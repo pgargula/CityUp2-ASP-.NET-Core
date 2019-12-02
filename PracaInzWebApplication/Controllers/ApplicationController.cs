b92ebe75be43c7a28705c81c88aec366634caa00
Piotr <Piotr@DINO>
2019-12-03 00:49:12 +0100

    create addApplication Form

--- PracaInzWebApplication/Controllers/ApplicationController.cs
+++ PracaInzWebApplication/Controllers/ApplicationController.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PracaInzWebApplication.CustomAtributes;
+using PracaInzWebApplication.Helpers;
+using PracaInzWebApplication.Models.ViewModels;
 
 namespace PracaInzWebApplication.Controllers
 {
     public class ApplicationController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly HttpClient _httpClient = new HttpClient();
+        public ApplicationController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
         [Authorize]
         public IActionResult UserApplications()
         {
@@ -17,6 +27,20 @@ namespace PracaInzWebApplication.Controllers
         [Authorize]
         public async Task<IActionResult> ApplicationsMap()
         {
+            return View();
+        }
+        [Authorize(Roles.User)]
+        [HttpGet]
+        public async Task<IActionResult> AddNew()
+        {
+            return View();
+        }
+
+        [Authorize(Roles.User)]
+        [HttpPost]
+        public async Task<IActionResult> AddNew(AddApplication addApplication)
+        {
+
             return View();
         }
     }
---
+++ PracaInzWebApplication/Models/ViewModels/AddApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracaInzWebApplication.Models.ViewModels
+{
+    public class AddApplication
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CityId { get; set; }
+        public int CategoryId { get; set; }
+        public string Street { get; set; }
+    }
+}
